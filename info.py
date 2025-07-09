733e70db0a6418d585cebe3d6d95e0ad09ad8b04
Chessman1184 <anjoriniyanuoluwa08@gmail.com>
2025-07-09 18:29:28 +0100

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mfilerequest')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/moviesupdatebot01')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Farmersbride')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/iboxsupportgroup') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/ph0enix_web')
 
